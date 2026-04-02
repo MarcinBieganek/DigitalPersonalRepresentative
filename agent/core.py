4bcd284911c64f582bfaf867b9672e960edb8b1e
MarcinBieganek <marcin.bieganek@gmail.com>
2026-04-02 15:43:50 +0200

    Simple chat function added.

--- agent/core.py
+++ agent/core.py
@@ -4,5 +4,16 @@ from openai import OpenAI
 load_dotenv(override=True)
 
 class Agent:
-    def __init__(self):
+    def __init__(self, model="gpt-4o-mini"):
         self.client = OpenAI()
+        self.model = model
+
+    def chat(self, message):
+        messages = [{"role": "user", "content": message}]
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+        )
+        
+        return response.choices[0].message.content
+
