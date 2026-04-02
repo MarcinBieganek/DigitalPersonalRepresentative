2cdde482035784d50075341c97b64370f9e9d96c
MarcinBieganek <marcin.bieganek@gmail.com>
2026-04-02 15:54:20 +0200

    Simple system prompt added.

--- agent/core.py
+++ agent/core.py
@@ -8,12 +8,20 @@ class Agent:
         self.client = OpenAI()
         self.model = model
 
+    def system_prompt(self):
+        system_prompt = f"You are acting as helpful assistant. You are answering questions in the chat, \
+particularly questions related to one indvidual who you are representing. \
+Your responsibility is to represent him for interactions in the chat as faithfully as possible. \
+Be professional and engaging, as if talking to a potential client or future employer."
+
+        return system_prompt
+
     def chat(self, message):
-        messages = [{"role": "user", "content": message}]
+        messages = [{"role": "system", "content": self.system_prompt()}] + [{"role": "user", "content": message}]
         response = self.client.chat.completions.create(
             model=self.model,
             messages=messages,
         )
-        
+
         return response.choices[0].message.content
 
