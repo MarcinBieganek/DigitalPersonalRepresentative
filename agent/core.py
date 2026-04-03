bd8a6edaf61093a588f02d3bce14b31ed5da69b8
MarcinBieganek <marcin.bieganek@gmail.com>
2026-04-03 12:55:47 +0200

    Person name configuration added to the Agent's code.

--- agent/core.py
+++ agent/core.py
@@ -1,22 +1,23 @@
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from .data_loader import load_text_file
+from data_loader import load_text_file
 
 load_dotenv(override=True)
 
 class Agent:
-    def __init__(self, model="gpt-4o-mini"):
+    def __init__(self, person_name, model="gpt-4o-mini"):
         self.client = OpenAI()
         self.model = model
+        self.person_name = person_name
         self.summary = load_text_file("data/summary.txt")
 
     def system_prompt(self):
-        system_prompt = f"You are acting as helpful assistant. You are answering questions in the chat, \
-particularly questions related to one indvidual who you are representing. \
-Your responsibility is to represent him for interactions in the chat as faithfully as possible. \
+        system_prompt = f"You are acting as helpful assistant representing {self.person_name}. You are answering questions in the chat, \
+particularly questions related to one individual who you are representing. \
+Your responsibility is to represent {self.person_name} for interactions in the chat as faithfully as possible. \
 Be professional and engaging, as if talking to a potential client or future employer. \
-You are given a summary of the individual's background. Use it to answer questions."
+You are given a summary of the {self.person_name} background. Use it to answer questions."
         system_prompt += f"\n\n## Summary:\n{self.summary}\n\n"
         system_prompt += f"With this context, please chat with the user, always staying professional."
 
