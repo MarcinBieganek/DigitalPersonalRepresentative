205670aef99af0c76c22d3fbdd83b5bfc844a9c4
MarcinBieganek <marcin.bieganek@gmail.com>
2026-04-03 13:10:04 +0200

    Hndling PDF file with data added.

--- agent/core.py
+++ agent/core.py
@@ -1,16 +1,17 @@
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from data_loader import load_text_file
+from data_loader import *
 
 load_dotenv(override=True)
 
 class Agent:
-    def __init__(self, person_name, model="gpt-4o-mini"):
+    def __init__(self, person_name, model="gpt-4o-mini", summary_path="data/summary.txt", linkedin_path="data/linkedin.pdf"):
         self.client = OpenAI()
         self.model = model
         self.person_name = person_name
-        self.summary = load_text_file("data/summary.txt")
+        self.summary = load_text_file(summary_path)
+        self.linkedin = load_pdf_text(linkedin_path)
 
     def system_prompt(self):
         system_prompt = f"You are acting as helpful assistant representing {self.person_name}. You are answering questions in the chat, \
@@ -18,7 +19,7 @@ particularly questions related to one individual who you are representing. \
 Your responsibility is to represent {self.person_name} for interactions in the chat as faithfully as possible. \
 Be professional and engaging, as if talking to a potential client or future employer. \
 You are given a summary of the {self.person_name} background. Use it to answer questions."
-        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n"
+        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n \n\n## LinkedIn:\n{self.linkedin}"
         system_prompt += f"With this context, please chat with the user, always staying professional."
 
         return system_prompt
--- agent/data_loader.py
+++ agent/data_loader.py
@@ -1,4 +1,14 @@
+from pypdf import PdfReader
 
 def load_text_file(path):
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
+
+def load_pdf_text(path):
+    reader = PdfReader(path)
+    text = ""
+    for page in reader.pages:
+        if content := page.extract_text():
+            text += content
+    return text
+
